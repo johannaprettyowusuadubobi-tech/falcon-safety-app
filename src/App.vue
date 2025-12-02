af04b615fd20b16348b68865e51526dcb547e380
johannaprettyowusuadubobi-tech <johannaprettyowusuadubobi@gmail.com>
2025-12-02 13:52:38 +0200

    new upgrade phase 1

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,28 @@
         </h1>
       </v-toolbar-title>
 
-      <v-spacer></v-spacer>
+      <v-btn icon
+        color="grey darken-3"
+       @click.stop="drawer = !drawer"
+       class="cyan--text"
+      >
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+          <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-divider></v-divider>
+
       <v-btn
        color="grey darken-3"
-       @click="login"
+       @click="login()"
        class="cyan--text"
       >
         login
       </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
+    </v-navigation-drawer>
       <v-menu
         left
         bottom
@@ -45,9 +55,11 @@
 export default {
   name: 'App',
 
-  data: () => ({
-    
-  }),
+  data () {
+    return {
+        drawer: null,
+    }
+  },
   methods: {
     login(){
       this.$router.push({path: "login"})
